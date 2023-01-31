a383d595ba3f58b04247b144e6908a06bc643d90
Bestic <bestic88@gmail.com>
2023-01-31 15:19:31 +0100

    [RD-50] [RD-57] Import BPNM file (project), Create new project

--- api/src/app/project/project.controller.ts
+++ api/src/app/project/project.controller.ts
@@ -1,20 +1,38 @@
 import {
+    Body,
     Controller,
     Get,
     Param,
+    Post,
+    UploadedFile,
+    UseInterceptors,
 } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CurrentUser } from '../common/decorators/user.decorator';
 import { User } from '../shared/user/user.entity';
 import { Project } from './project.entity';
 import { ProjectByUuidPipe } from './pipes/project-by-uuid.pipe';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { Express } from 'express';
 
 @Controller('project')
 export class ProjectController {
     constructor(private readonly projectService: ProjectService) {}
 
+    @Post('')
+    public async create(
+        @Body() createProjectDto: CreateProjectDto,
+        @CurrentUser() user: User,
+    ): Promise<Project> {
+        return this.projectService.create(createProjectDto, user);
+    }
+
     @Get('user/:uuid')
-    public async displayUsersProjects(@CurrentUser() user: User): Promise<Project[]> {
+    public async displayUsersProjects(
+        @CurrentUser() user: User,
+    ): Promise<Project[]> {
         return user.getProjects();
     }
 
@@ -22,6 +40,31 @@ export class ProjectController {
     public async projectDetail(
         @Param('uuid', ProjectByUuidPipe) project: Project,
     ): Promise<Project> {
+        return project;
+    }
+    @Post('import')
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: diskStorage({
+                destination: function (req, file, cb) {
+                    cb(null, './public/data/uploads/');
+                },
+                filename: function (req, file, cb) {
+                    cb(null, file.originalname);
+                },
+            }),
+        }),
+    )
+    async uploadedFile(
+        @Body() createProjectDto: CreateProjectDto,
+        @CurrentUser() user: User,
+        @UploadedFile() file: Express.Multer.File,
+    ) {
+        createProjectDto.path = file.path;
+        const project = await this.projectService.create(
+            createProjectDto,
+            user,
+        );
         return project;
     }
 }
--- api/src/app/project/project.service.ts
+++ api/src/app/project/project.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { ProjectRepository } from './project.repository';
 import { Project } from './project.entity';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { User } from '../shared/user/user.entity';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectService {
     public constructor(private readonly projectRepository: ProjectRepository) {}
 
     public async getOneByUuid(uuid: string): Promise<Project> {
-        return this.projectRepository.findOneOrFail({ uuid: uuid });
+        return await this.projectRepository.findOneOrFail({ uuid: uuid });
+    }
+
+    public async create(
+        createProjectDto: CreateProjectDto,
+        user: User,
+    ): Promise<Project> {
+        const project = Project.create(createProjectDto, user);
+        this.projectRepository.persist(project);
+        await this.projectRepository.flush();
+
+        return project;
+    }
+
+    public async update(
+        project: Project,
+        updateProjectDto: UpdateProjectDto,
+    ): Promise<Project> {
+        project.update(updateProjectDto);
+        await this.projectRepository.flush();
+        return project;
     }
 }
