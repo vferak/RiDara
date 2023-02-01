99ff377f642aa472bec423e9a7deb370c9e41583
Vojtěch Ferák <vferak@gmail.com>
2023-02-01 18:06:51 +0100

    Basic BPMN Modeler implementation

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    css: ['/node_modules/css.gg/icons/all.css'],
+    css: [
+        '/node_modules/css.gg/icons/all.css',
+        '/node_modules/bpmn-js/dist/assets/diagram-js.css',
+        '/node_modules/bpmn-js/dist/assets/bpmn-font/css/bpmn.css',
+        '/node_modules/bpmn-js-properties-panel/dist/assets/properties-panel.css',
+        '/node_modules/bpmn-js-properties-panel/dist/assets/element-templates.css'
+    ],
     modules: [
         '@nuxtjs/tailwindcss',
         '@vueuse/nuxt',
