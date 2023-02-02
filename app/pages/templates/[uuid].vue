5878be6d1195c422e406ebcc8970ebf15bcae56f
Vojtěch Ferák <vferak@gmail.com>
2023-02-02 13:00:49 +0100

    [UX] Confirm route leave in template detail

--- app/pages/templates/[uuid].vue
+++ app/pages/templates/[uuid].vue
@@ -1,6 +1,16 @@
 <script setup lang='ts'>
 const { data: xmlBlob } = await useFetch<Blob>('/diagram.bpmn');
 const xml = await xmlBlob.value?.text();
+
+onBeforeRouteLeave((to, from, next) => {
+    const confirmed = confirm('Are you sure you want to leave? All unsaved progress will be lost.');
+    if (!confirmed) {
+        next(false);
+        return;
+    }
+
+    next();
+});
 </script>
 
 <template>
