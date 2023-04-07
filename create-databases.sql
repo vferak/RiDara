04b333f72e964022c2c125c54f682d8329db1d73
Vojtěch Ferák <vferak@gmail.com>
2023-04-07 18:56:00 +0200

    Installed second database for testing purposes

---
+++ create-databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ridara`;
+GRANT ALL PRIVILEGES ON ridara.* TO 'user'@'%';
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ridara_e2e_test`;
+GRANT ALL PRIVILEGES ON ridara_e2e_test.* TO 'user'@'%';
