828df5bcd47ba53a31c97e24e310e7a885d6e7a5
Vojtěch Ferák <vferak@gmail.com>
2023-04-09 10:10:42 +0200

    [RD-113] Fixed BPMN stylesheets

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,16 +4,14 @@ export default defineNuxtConfig({
     css: [
         '/node_modules/css.gg/icons/all.css',
         '/node_modules/bpmn-js/dist/assets/diagram-js.css',
+        '/node_modules/bpmn-js/dist/assets/bpmn-js.css',
         '/node_modules/bpmn-js/dist/assets/bpmn-font/css/bpmn.css',
         '/node_modules/bpmn-js-properties-panel/dist/assets/properties-panel.css',
     ],
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@vueuse/nuxt',
-    ],
+    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
     runtimeConfig: {
         public: {
-            API_URL: 'Missing .env config file!'
-        }
-    }
-})
+            API_URL: 'Missing .env config file!',
+        },
+    },
+});
