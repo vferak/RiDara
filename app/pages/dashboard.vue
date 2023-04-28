4f206941c85d86f7ebc02bd27e8518ad0f163a53
Bestic <bestic88@gmail.com>
2023-04-28 12:56:26 +0200

    [RD-66] Dashboard edit

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -2,8 +2,8 @@
 const { getWorkspaces } = useWorkspace();
 const { getUser } = useUser();
 
-const date = useDateFormat(useNow(), 'dddd, MMMM DD, YYYY')
-const time = useDateFormat(useNow(), 'HH:mm:ss')
+const date = useDateFormat(useNow(), 'dddd, MMMM DD, YYYY', { locales: 'en-US' })
+const time = useDateFormat(useNow(), 'HH:mm:ss', { locales: 'en-US' })
 
 const { data: user } = await getUser();
 
@@ -14,23 +14,30 @@ const lastName = user.value?.lastName;
 const exist = computed(() => workspaces.value !== null && workspaces.value?.length === 0);
 </script>
 <template>
-    <LayoutGridList title='Dashboard'>
-        <template v-slot:alerts>
-            <Alert>
-                Welcome back {{ firstName }} {{ lastName }}, today is {{ date }} and it's {{ time }}.
-            </Alert>
-            <AlertInform v-if='exist' class='mt-4'>
-                <div class="flex-1">
-                    <span>Hi, it looks like you haven't created a workspace yet. You can create one by clicking the following button.</span>
-                </div>
-                <div class="flex-none">
-                    <NuxtLink to='/workspaces' class="btn btn-sm btn-primary">Create workspace</NuxtLink>
-                </div>
-            </AlertInform>
-        </template>
+    <div class="hero py-16 bg-primary text-primary-content">
+        <div class="hero-content text-center">
+            <div class="max-w-2xl">
+                <h1 class="text-6xl font-bold">Welcome to RiDara</h1>
+                <p class="text-3xl py-16">This tool allows semiformal process modeling based on formal restrictions from Unified Process Meta Model.</p>
+                <NuxtLink to='/workspaces' class="btn btn-secondary btn-lg">Start now</NuxtLink>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto my-6">
+        <Alert class='my-4'>
+            Welcome back {{ firstName }} {{ lastName }}, today is {{ date }} and it's {{ time }}.
+        </Alert>
 
-        <template v-slot:grid>
+        <AlertInform v-if='exist'>
+            <div class="flex-1">
+                <span>Hi, it looks like you haven't created a workspace yet. You can create one by clicking the following button.</span>
+            </div>
+            <div class="flex-none">
+                <NuxtLink to='/workspaces' class="btn btn-sm btn-primary">Create workspace</NuxtLink>
+            </div>
+        </AlertInform>
+        <div class='grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-4 gap-8 mt-8'>
             <CardsWorkspaceCard v-for='workspace in workspaces' :key='workspace.id' :workspace='workspace' />
-        </template>
-    </LayoutGridList>
+        </div>
+    </div>
 </template>
